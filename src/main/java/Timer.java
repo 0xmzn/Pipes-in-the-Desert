0b8c9e491a4c9e209a1d554cb70eab1757d80350
Azam Rakhmatillaev <azam.rahmatillaev@gmail.com>
2024-03-24 23:11:31 +0100

    Updated Timer.java class

--- src/main/java/Timer.java
+++ src/main/java/Timer.java
@@ -1,2 +1,13 @@
+import static java.lang.System.out;
+
 public class Timer {
+    private static void printMethodName(String methodName) {
+        out.println("\n------------------------------------------------------------");
+        out.println(methodName + " method of the Timer class is called.");
+        out.println("------------------------------------------------------------\n");
+    }
+
+    public static void startTimer() {
+        printMethodName("startTimer()");
+    }
 }
