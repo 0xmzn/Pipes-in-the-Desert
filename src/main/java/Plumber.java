99181739f39b63f50e7a05bbf28f252aba0d797b
Nia Tatrishvili <nia.tatrishvili@edu.bme.hu>
2024-03-24 15:42:38 +0100

    adds pickUpPump, installPump and placePipeEnd

--- src/main/java/Plumber.java
+++ src/main/java/Plumber.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Set;
 
 public class Plumber extends Player 
 {
@@ -89,11 +90,27 @@ public class Plumber extends Player
         return true;    
     }
 
-    public void pickUp(){
-        printMethodName("pickUp");
+    private boolean getManufacturedElement() {
+        printMethodName("getManufacturedElement()");
+        int isManufactured = askQuestion("Does the cistern have a manufactured pump?");
+        
+        if (isManufactured != 1 && isManufactured != 2){
+            System.out.println("Invalid command, pickUp() failed");
+            return false;
+        }
+        else if (isManufactured == 2){
+            return false;
+        }
+
+        System.out.println("");
+        return true;    
+    }
+
+    public void pickUpPipeEnd(){
+        printMethodName("pickUpPipeEnd");
         
         System.out.println("You are trying to pick up a pipe end...");
-        if (!checkType() || !checkInventory() || !getLocation()){
+        if (!checkType() || !checkInventory() || !getLocation() || !getManufacturedElement()){
             return;
         }
         boolean isConnectedToActiveElement = getConnected();
@@ -106,15 +123,73 @@ public class Plumber extends Player
         }
 
         System.out.println("Pipe picked up successfully!");
+    }
+
+    public void pickUpPump(){
+        printMethodName("pickUpPump");
+        
+        System.out.println("You are trying to pick up a pump...");
+        if (!checkType() || !checkInventory() || !getLocation() || !getManufacturedElement()){
+            return;
+        }
+        
+        System.out.println("Pump picked up successfully!");
 
     }
 
+    private void setLocation() {
+        printMethodName("setLocation");
+    }
+
+    private void cutPipe() {
+        printMethodName("cutPipe");
+    }
+
+    private void removeInventory() {
+        printMethodName("removeInventory");
+
+    }
     public void installPump(){
         printMethodName("installPump");
+
+        System.out.println("You are trying to install a pump into the system...");
+        if (!checkType() || !checkInventory() 
+        || !getLocation() || !getManufacturedElement()){
+            return;
+        }
+
+        setLocation();
+        cutPipe();
+        connect();
+        setInputPipe();
+        setOutputPipe();
+        removeInventory();
+        System.out.println("Pump picked up successfully!");
+
     }
 
     public void placePipeEnd(){
         printMethodName("placePipeEnd");
+
+
+        System.out.println("You are trying to place a pipe end into the system...");
+        if (!checkType() || !checkInventory() || !getConnected()){
+            return;
+        }
+
+        boolean isConnectedToActiveElement = getConnected();
+        if (!isConnectedToActiveElement){
+            connect();
+        }
+        else if (isConnectedToActiveElement && !getPipeEnds()){
+            System.err.println("Pick up failed as the other end of pipe is also disconnected!");
+            return;
+        }
+
+        setLocation();
+        removeInventory();
+
+        System.out.println("Pump picked up successfully!");
     }
 
     public void connect(){
