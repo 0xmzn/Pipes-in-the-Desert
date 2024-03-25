9a0a79b13e0255be5a3e98d0df3af8365d530150
Nia Tatrishvili <nia.tatrishvili@edu.bme.hu>
2024-03-25 11:04:48 +0100

    removed duplicate methods from parent, adds formatting

--- src/main/java/Plumber.java
+++ src/main/java/Plumber.java
@@ -1,175 +1,51 @@
+import static java.lang.System.out;
 import java.util.Scanner;
 import java.util.Set;
 
 public class Plumber extends Player 
 {
-    Scanner scanner;
 
     public Plumber(){
         super();
-        scanner = new Scanner(System.in);
     }
 
-    private static void printMethodName(String methodName){
-        System.out.println("\n--------------------");
-        System.out.println(methodName);
-        System.out.println("--------------------\n");
-    }
-
-    private int askQuestion(String question){
-        System.out.println(question);
-        System.out.println("1. Yes");
-        System.out.println("2. No");
-
-        return scanner.nextInt();
-    }
-
-    private boolean checkType(){
-        printMethodName("checkType()");
-        int isPlumber = askQuestion("Is the curret player a plumber?");
-
-        if (isPlumber != 1 && isPlumber != 2){
-            System.out.println("Invalid command, pickUp() failed");
-            return false;
-        }
-        else if (isPlumber == 2){
-            System.out.println("This player is not a plumber, pickUp() failed");
-            return false;
-        }
-
-        System.out.println("");
-        return true;
-    }
-
-    private boolean checkInventory(){
-        printMethodName("checkInventory()");
-        int isInventoryFree = askQuestion("Is the inventory free?");
-        
-        if (isInventoryFree != 1 && isInventoryFree != 2){
-            System.out.println("Invalid command, pickUp() failed");
-            return false;
-        }
-        else if (isInventoryFree == 2){
-            System.out.println("You can not pick up multiple items simultaneously, pickUp() failed");
-            return false;
-        }
-
-        System.out.println("");
-        return true;
-        
-    }
-
-    private boolean getConnected() {
-        printMethodName("getConnected()");
-        int isConnected = askQuestion("Is the pipe end connected to an element?");
-        
-        if (isConnected != 1 && isConnected != 2){
-            System.out.println("Invalid command, pickUp() failed");
-            return false;
-        }
-        else if (isConnected == 2){
-            return false;
-        }
-        return true;
-    }
-
-    private boolean getPipeEnds() {
-        printMethodName("getPipeEnds()");
-        int hasOtherEnd = askQuestion("Is the other pipe end connected to an active element?");
-        
-        if (hasOtherEnd != 1 && hasOtherEnd != 2){
-            System.out.println("Invalid command, pickUp() failed");
-            return false;
-        }
-        else if (hasOtherEnd == 2){
-            System.out.println("The other end of this pipe is not connected to any active elements! PickUp() failed");
-            return false;
-        }
-
-        System.out.println("");
-        return true;    
-    }
-
-    private boolean getManufacturedElement() {
-        printMethodName("getManufacturedElement()");
-        int isManufactured = askQuestion("Does the cistern have a manufactured pump?");
-        
-        if (isManufactured != 1 && isManufactured != 2){
-            System.out.println("Invalid command, pickUp() failed");
-            return false;
-        }
-        else if (isManufactured == 2){
-            return false;
-        }
-
-        System.out.println("");
-        return true;    
-    }
-
-    private boolean getLocation(String location) {
-        printMethodName("getLocation()");
-        int isLocation = askQuestion("Is the player standing on a "+location+"?");
-        
-        if (isLocation != 1 && isLocation != 2){
-            System.out.println("Invalid command, operation failed");
-            return false;
-        }
-        else if (isLocation == 2){
-            System.err.println("You are not standing on a "+location+"! Operation failed");
-            return false;
-        }
-
-        System.out.println("");
-        return true;
-    }
-
+    
     public void pickUpPipeEnd(){
         printMethodName("pickUpPipeEnd");
         
-        System.out.println("You are trying to pick up a pipe end...");
-        if (!checkType() || !checkInventory() || !getLocation("pipe end") || !getManufacturedElement()){
+        out.println("You are trying to pick up a pipe end...");
+        if (!checkType("Plumber") || !checkInventory() || !getLocation("pipe end") || !getManufacturedElement()){
             return;
         }
         boolean isConnectedToActiveElement = getConnected();
         if (!isConnectedToActiveElement){
             disconnect();
         }
-        else if (isConnectedToActiveElement && !getPipeEnds()){
-            System.err.println("Pick up failed as the other end of pipe is also disconnected!");
+        else if (!getPipeEnds()){
+            out.println("Pick up failed as the other end of pipe is also disconnected!");
             return;
         }
 
-        System.out.println("Pipe picked up successfully!");
+        out.println("Pipe picked up successfully!");
     }
 
     public void pickUpPump(){
         printMethodName("pickUpPump");
         
-        System.out.println("You are trying to pick up a pump...");
-        if (!checkType() || !checkInventory() || !getLocation("pump") || !getManufacturedElement()){
+        out.println("You are trying to pick up a pump...");
+        if (!checkType("Plumber") || !checkInventory() || !getLocation("pump") || !getManufacturedElement()){
             return;
         }
         
-        System.out.println("Pump picked up successfully!");
+        out.println("Pump picked up successfully!");
 
     }
 
-    private void setLocation() {
-        printMethodName("setLocation");
-    }
-
-    private void cutPipe() {
-        printMethodName("cutPipe");
-    }
-
-    private void removeInventory() {
-        printMethodName("removeInventory");
-    }
     public void installPump(){
         printMethodName("installPump");
 
-        System.out.println("You are trying to install a pump into the system...");
-        if (!checkType() || !checkInventory() 
+        out.println("You are trying to install a pump into the system...");
+        if (!checkType("plumber") || !checkInventory() 
         || !getLocation("pump") || !getManufacturedElement()){
             return;
         }
@@ -180,15 +56,14 @@ public class Plumber extends Player
         setInputPipe();
         setOutputPipe();
         removeInventory();
-        System.out.println("Pump picked up successfully!");
-
+        out.println("Pump installed successfully!");
     }
 
     public void placePipeEnd(){
         printMethodName("placePipeEnd");
 
-        System.out.println("You are trying to place a pipe end into the system...");
-        if (!checkType() || !checkInventory() || !getConnected()){
+        out.println("You are trying to place a pipe end into the system...");
+        if (!checkType("plumber") || !checkInventory() || !getConnected()){
             return;
         }
 
@@ -196,15 +71,15 @@ public class Plumber extends Player
         if (!isConnectedToActiveElement){
             connect();
         }
-        else if (isConnectedToActiveElement && !getPipeEnds()){
-            System.err.println("Pick up failed as the other end of pipe is also disconnected!");
+        else if (!getPipeEnds()){
+            out.println("Pick up failed as the other end of pipe is also disconnected!");
             return;
         }
 
         setLocation();
         removeInventory();
 
-        System.out.println("Pump picked up successfully!");
+        out.println("Pipe end placed successfully!");
     }
 
     public void connect(){
@@ -218,8 +93,8 @@ public class Plumber extends Player
     public void fixPipe(){
         printMethodName("placePipeEnd");
 
-        System.out.println("You are trying to fix a pipe...");
-        if (!checkType() || !isPunctured() || !getLocation()){
+        out.println("You are trying to fix a pipe...");
+        if (!checkType("plumber") || !isPunctured() || !getLocation("pipe")){
             return;
         }
 
@@ -227,18 +102,95 @@ public class Plumber extends Player
         if (!isConnectedToActiveElement){
             connect();
         }
-        else if (isConnectedToActiveElement && !getPipeEnds()){
-            System.err.println("Pick up failed as the other end of pipe is also disconnected!");
+        else if (!getPipeEnds()){
+            out.println("Pick up failed as the other end of pipe is also disconnected!");
             return;
         }
 
         setLocation();
         removeInventory();
 
-        System.out.println("Pump picked up successfully!");
+        out.println("Pipe fixed successfully!");
     }
 
     public void fixPump(){
         printMethodName("fixPump");
+    }
+
+    private boolean checkInventory(){
+        printMethodName("checkInventory()");
+        int isInventoryFree = askQuestion("Is the inventory free?");
+        
+        if (isInventoryFree != 1 && isInventoryFree != 2){
+            out.println("Invalid command, operation failed");
+            return false;
+        }
+        else if (isInventoryFree == 2){
+            out.println("You can not pick up multiple items simultaneously, operation failed");
+            return false;
+        }
+
+        out.println("");
+        return true;
+        
+    }
+
+    private boolean getConnected() {
+        printMethodName("getConnected()");
+        int isConnected = askQuestion("Is the pipe end connected to an element?");
+        
+        if (isConnected != 1 && isConnected != 2){
+            out.println("Invalid command, operation failed");
+            return false;
+        }
+        else if (isConnected == 2){
+            return false;
+        }
+        return true;
+    }
+
+    private boolean getPipeEnds() {
+        printMethodName("getPipeEnds()");
+        int hasOtherEnd = askQuestion("Is the other pipe end connected to an active element?");
+        
+        if (hasOtherEnd != 1 && hasOtherEnd != 2){
+            out.println("Invalid command, operation failed");
+            return false;
+        }
+        else if (hasOtherEnd == 2){
+            out.println("The other end of this pipe is not connected to any active elements! operation failed");
+            return false;
+        }
+
+        out.println("");
+        return true;    
+    }
+
+    private boolean getManufacturedElement() {
+        printMethodName("getManufacturedElement()");
+        int isManufactured = askQuestion("Does the cistern have a manufactured pump?");
+        
+        if (isManufactured != 1 && isManufactured != 2){
+            out.println("Invalid command, operation failed");
+            return false;
+        }
+        else if (isManufactured == 2){
+            return false;
+        }
+
+        out.println("");
+        return true;    
+    }
+
+    private void setLocation() {
+        printMethodName("setLocation");
+    }
+
+    private void cutPipe() {
+        printMethodName("cutPipe");
+    }
+
+    private void removeInventory() {
+        printMethodName("removeInventory");
     }
 }
