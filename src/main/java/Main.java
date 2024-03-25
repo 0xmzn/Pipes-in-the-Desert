638711a734aaed7d482ef3c0ab9694b4f746d146
Nia Tatrishvili <nia.tatrishvili@edu.bme.hu>
2024-03-25 09:45:39 +0100

    calls startgame from main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        //Controller c = new Controller();
-        //c.displayMenu();
-        Player p = new Saboteur();
-        p.changeInputPipe();
+        Controller c = new Controller();
+        c.displayMenu();
     }
 }
