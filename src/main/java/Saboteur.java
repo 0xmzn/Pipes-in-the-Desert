53d84e40ec8f3b1edf3bf12a3dfc4125743e17aa
Nia Tatrishvili <nia.tatrishvili@edu.bme.hu>
2024-03-25 09:34:56 +0100

    removes reimplementation of inherited methods, fixes formatting

--- src/main/java/Saboteur.java
+++ src/main/java/Saboteur.java
@@ -1,34 +1,19 @@
-import java.util.Scanner;
 import static java.lang.System.out;
 
 public class Saboteur extends Player {
 
-    private Scanner scanner;
-
-    public Saboteur(){
-        scanner = new Scanner(System.in);
-    }
-
-    private static void printMethodName(String methodName){
-        out.println("\n------------------------------------------------------------");
-        out.println(methodName + " method of the Controller class is called.");
-        out.println("------------------------------------------------------------\n");
-    }
-
-    private int askQuestion(String question){
-        System.out.println(question);
-        System.out.println("1. Yes");
-        System.out.println("2. No");
-
-        return scanner.nextInt();
+    public Saboteur() {
+        super();
     }
 
     public void puncturePipe() {
         printMethodName("puncturePipe()");
-        if (getLocation() && !isPunctured()) {
-            printMethodName("Saboteur is on the pipe and it's not punctured. Puncturing Pipe...");
+
+        if (getLocation("Pipe") && !isPunctured()) {
+            out.println("Saboteur is on the pipe and it's not punctured. Puncturing Pipe...");
+            out.println("Pipe punctured successfully!");
         } else {
-            printMethodName("Saboteur is not on the pipe or the pipe is punctured. Cannot puncture pipe.");
-        } 
+            out.println("Saboteur is not on the pipe or the pipe is punctured. Cannot puncture pipe.");
+        }
     }
 }
